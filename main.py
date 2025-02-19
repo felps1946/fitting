327755ed44676d5028c6bf5232e5487b2c7060b4
felps1946 <felipesilva.s@ufba.br>
2025-02-18 22:22:14 -0300

    fix code

--- main.py
+++ main.py
@@ -5,58 +5,27 @@ Fitting Non-Linear Curve
 Autor: Felipe Silva
 """
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
-import scipy
-from scipy import optimize
-import math
+import matplotlib.pyplot as plt
 
-#Importar dados de um txt ------------
-
-#Leitura dos dados 
 dados = pd.read_csv("data.txt", sep="\t", header=0).to_numpy()
-    
-#definir as variaveis que serao plotadas
-V = dados[: , 0]
-I = dados[: , 1]
+V = dados[:, 0]
+I = dados[:, 1]
 
+grau = 6
 
-#Fitting ----------------------------
+coeficientes = np.polyfit(V, I, grau)
 
-def f(V, IL, IS, RS, RP):
-    k = 8.617e-5
-    T = 300
-    a = k*T
-    IL = 5e-3
-    IS = 2e-18
-    RS = 10
-    RP = 1e4
-    return IL-IS*(math.exp(V+(I*RS))/a-1)-(V+(I*RS))/RP
+I_fit = np.polyval(coeficientes, V)
 
-guess = [0.12,1e-11,0.0,1e5]
+print("Coeficientes do polinômio:", coeficientes)
 
-params, params_covariance = scipy.optimize.curve_fit(f, V, I, guess)
-IL, IS, RS, RP = params 
-
-
-#Plot -------------------------------
-
-#Plotagem dos dados
-plt.scatter(V, I, color = 'black', label = 'dados experimentais')
-
-#Plotagem da curva de tendência
-V = np.linspace(0.0,max(V),49)
-plt.plot(V, f(V, IL, IS, RS, RP), color="blue", label = 'Curva ajustada')
-
-#Títulos e legendas
-plt.legend(loc='best')
-
-plt.xlabel('V(mV)', size = 10)
-
-plt.ylabel("I(mA)", size = 10)
-
-plt.title('Curva IxV', size = 15) 
-
-#salvando a  figura -------------------
-
-plt.savefig('curva-teste-graph', dpi = 300)
+plt.figure(figsize=(10, 6))
+plt.scatter(V, I, label='Dados experimentais', color='red')
+plt.plot(V, I_fit, label=f'Ajuste polinomial (grau {grau})', color='blue')
+plt.xlabel('Tensão (V)')
+plt.ylabel('Corrente (I)')
+plt.title('Fitting I-V Curve')
+plt.legend()
+plt.grid()
+plt.show()
